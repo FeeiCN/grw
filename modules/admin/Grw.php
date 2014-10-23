f30961696d03e068e872f3392002c15155feb75b
DongYuxiang <dongm2ez@163.com>
2014-10-24 00:03:09 +0800

    登录修正

--- modules/admin/Grw.php
+++ modules/admin/Grw.php
@@ -128,7 +128,7 @@ class Grw extends FeiController
 //                $this->jump('http://www.grw.name/index.php?c=Grw&a=login');
 
 			#qq login 开发人员离线开发请注释，并在数据库fei_user表中添加一个条新数据，使username, password字段有值
-			//FeiClass('FeiQQlogin')->login();
+			FeiClass('FeiQQlogin')->login();
 		}
 	}
 
